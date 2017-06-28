ced777b82e7a18a5bdae41c76f500aa8a2654e60
Arthur Arnaud <arthur.arnaud@epitech.eu>
2017-06-28 22:38:52 +0000

    ...

--- client/include/ai/AState.hpp
+++ client/include/ai/AState.hpp
@@ -12,6 +12,22 @@
 
 namespace ai
 {
+  enum BasicState : std::int8_t
+  {
+    BROADCAST,
+    CONNECT_NBR,
+    EJECT,
+    FORK,
+    FORWARD,
+    INCANTATION,
+    INVENTORY,
+    LEFT,
+    LOOK,
+    RIGHT,
+    SET,
+    TAKE,
+  };
+
   class AState : public IState
   {
   public:
@@ -24,7 +40,7 @@ namespace ai
     AState &operator=(AState &&);
 
   protected:
-    std::map<State, IState *> m_states;
+    std::map<BasicState, IState *> m_states;
     State m_curState;
     Value m_curValue;
   };
@@ -35,4 +51,3 @@ namespace ai
 #endif
 
 #endif // !ASTATE_HPP_
-
--- client/src/ai/AState.cpp
+++ client/src/ai/AState.cpp
@@ -2,8 +2,23 @@
 
 namespace ai
 {
+  static std::map<BasicState, std::unique_ptr<IState>> AState::m_states = {
+      {, std::ma}};
+
   AState::AState() : m_states(), m_curState(), m_curValue(Value::LOOP)
   {
+    m_states[BasicState::BROADCAST] = std::make_unique<BroadcastState>();
+    m_states[BasicState::CONNECT_NBR] = std::make_unique<ConnectnbrState>();
+    m_states[BasicState::EJECT] = std::make_unique<EjectState>();
+    m_states[BasicState::FORK] = std::make_unique<ForkState>();
+    m_states[BasicState::FORWARD] = std::make_unique<ForwardState>();
+    m_states[BasicState::INCANTATION] = std::make_unique<IncantationState>();
+    m_states[BasicState::INVENTORY] = std::make_unique<InventoryState>();
+    m_states[BasicState::LEFT] = std::make_unique<LeftState>();
+    m_states[BasicState::LOOK] = std::make_unique<LookState>();
+    m_states[BasicState::RIGHT] = std::make_unique<RightState>();
+    m_states[BasicState::SET] = std::make_unique<SetState>();
+    m_states[BasicState::TAKE] = std::make_unique<TakeState>();
   }
 
   AState::AState(AState const &that)
