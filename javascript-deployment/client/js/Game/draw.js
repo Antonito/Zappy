796024b1c19a844bcd07862b27dba5de4156df08
marwane <marwane.khsime@epitech.eu>
2017-07-01 00:43:52 +0200

    seems legit

---
+++ javascript-deployment/client/js/Game/draw.js
@@ -0,0 +1,156 @@
+function DrawAll() {
+
+    /* Draw Resources */
+    for (var y = 0; y < raw_map_resources.length; ++y) {
+
+        for (var x = 0; x < raw_map_resources[y].length; ++x) {
+
+            var cell = raw_map_resources[y][x];
+
+            DrawCellResources(cell, x, y);
+
+        }
+
+    }
+
+    /* Draw Mobs */
+    for (var i = 0; i < raw_mobs_list.length; ++i) {
+        DrawCellMob(raw_mobs_list[i]);
+    }
+
+    /* Draw Eggs */
+    for (var i = 0; i < raw_eggs_list.length; ++i) {
+        DrawCellEgg(raw_eggs_list[i]);
+    }
+
+}
+
+function DrawCellResources(cell_res, x, y) {
+
+    if (cell_res.food > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.FOOD.X,
+            y * 100 + SPRITES_POSITION.FOOD.Y, 'food');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.FOOD.SW,
+            SPRITES_POSITION.FOOD.SH
+        );
+
+    }
+
+    if (cell_res.linemate > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.LINEMATE.X,
+            y * 100 + SPRITES_POSITION.LINEMATE.Y, 'linemate');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.LINEMATE.SW,
+            SPRITES_POSITION.LINEMATE.SH
+        );
+
+    }
+
+    if (cell_res.deraumere > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.DERAUMERE.X,
+            y * 100 + SPRITES_POSITION.DERAUMERE.Y, 'deraumere');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.DERAUMERE.SW,
+            SPRITES_POSITION.DERAUMERE.SH
+        );
+
+    }
+
+    if (cell_res.sibur > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.SIBUR.X,
+            y * 100 + SPRITES_POSITION.SIBUR.Y, 'sibur');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.SIBUR.SW,
+            SPRITES_POSITION.SIBUR.SH
+        );
+
+    }
+
+    if (cell_res.mendiane > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.MENDIANE.X,
+            y * 100 + SPRITES_POSITION.MENDIANE.Y, 'mendiane');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.MENDIANE.SW,
+            SPRITES_POSITION.MENDIANE.SH
+        );
+
+    }
+
+    if (cell_res.phiras > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.PHIRAS.X,
+            y * 100 + SPRITES_POSITION.PHIRAS.Y, 'phiras');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.PHIRAS.SW,
+            SPRITES_POSITION.PHIRAS.SH
+        );
+
+    }
+
+    if (cell_res.thystame > 0) {
+
+        var image = resources_layer.create(
+            x * 100 + SPRITES_POSITION.THYSTAME.X,
+            y * 100 + SPRITES_POSITION.THYSTAME.Y, 'thystame');
+
+        ScaleImage(
+            image,
+            SPRITES_POSITION.THYSTAME.SW,
+            SPRITES_POSITION.THYSTAME.SH
+        );
+
+    }
+
+}
+
+function DrawCellMob(mob, x, y) {
+
+    var image = resources_layer.create(
+        mob.pos_x * 100 + SPRITES_POSITION.MOB.X,
+        mob.pos_y * 100 + SPRITES_POSITION.MOB.Y, MOBS_COLOR[mob.teamId % MOBS_COLOR.length]);
+
+    ScaleImage(
+        image,
+        SPRITES_POSITION.MOB.SW,
+        SPRITES_POSITION.MOB.SH
+    );
+
+}
+
+function DrawCellEgg(egg, x, y) {
+
+    var image = resources_layer.create(
+        egg.pos_x * 100 + SPRITES_POSITION.EGG.X,
+        egg.pos_y * 100 + SPRITES_POSITION.EGG.Y, 'egg');
+
+    ScaleImage(
+        image,
+        SPRITES_POSITION.EGG.SW,
+        SPRITES_POSITION.EGG.SH
+    );
+
+}
---
+++ javascript-deployment/client/js/main.js
@@ -0,0 +1,39 @@
+/*
+ **  Game Map Dimensions
+ */
+var GAME_WIDTH = 1000;
+var GAME_HEIGHT = 1000;
+
+/**
+ * WebSocket
+ */
+var ws;
+
+/*
+ **  Create Context
+ */
+var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS),
+    Main = function() {};
+
+Main.prototype = {
+
+    preload: function() {
+
+        game.load.script('menu', 'js/Menu/menu.js');
+        game.load.script('game', 'js/Game/game.js');
+
+    },
+
+    create: function() {
+        /*
+         **  Add states and run the menu 
+         */
+        game.state.add('MENU', Menu);
+        game.state.add('INGAME', InGame);
+        game.state.start('MENU');
+    },
+
+}
+
+game.state.add('Main', Main);
+game.state.start('Main');
