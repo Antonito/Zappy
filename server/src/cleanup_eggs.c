09f7835c01bdeeed92f16938ec4ce894a4e28d0c
Antoine Baché <antoine.bache@epitech.eu>
2017-06-30 16:47:25 +0200

    implemented eggs list

--- server/src/cleanup_eggs.c
+++ server/src/cleanup_eggs.c
@@ -5,13 +5,27 @@
 ** Login   <antoine.bache@epitech.net>
 **
 ** Started on  Fri Jun 30 16:32:46 2017 Antoine Baché
-** Last update Fri Jun 30 16:35:02 2017 Antoine Baché
+** Last update Fri Jun 30 16:45:36 2017 Antoine Baché
 */
 
 #include <assert.h>
+#include <stdlib.h>
 #include "zappy_cleanup.h"
 
-void		zappy_cleanup_eggs(t_zappy_egg_manager * const man)
+void			zappy_cleanup_eggs(t_zappy_egg_manager * const man)
 {
+  int32_t		i;
+  t_zappy_egg		*cur;
+  t_zappy_egg		*tmp;
+
   assert(man);
+  i = 0;
+  cur = man->eggs;
+  while (i < man->nb_eggs)
+    {
+      tmp = cur;
+      cur = cur->next;
+      free(tmp);
+      ++i;
+    }
 }
