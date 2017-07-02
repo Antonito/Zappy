be6c87a21a53eb6c042f60bf373b869175a42f66
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-07-02 23:32:21 +0200

    fix

--- server/src/vision.c
+++ server/src/vision.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.bache@epitech.net>
 **
 ** Started on  Mon Jun 26 17:31:40 2017 Antoine Baché
-** Last update Sun Jul  2 20:58:26 2017 Ludovic Petrenko
+** Last update Sun Jul  2 20:55:47 2017 Antoine Baché
 */
 
 #include <assert.h>
@@ -54,34 +54,9 @@ static void			zappy_vision_line(t_zappy_map const *
     {
       ndx = off - infos[2] + j;
       LOG(LOG_DEBUG, "=== VISION ===");
-      if (infos[4] == CLI_NORTH)
-	{
-	  LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0] + j,
-	      cli->game.y + infos[1]);
-	  map_tile = zappy_get_map_tile(map, cli->game.x + infos[0] + j,
-					cli->game.y + infos[1]);
-	}
-      else if (infos[4] == CLI_EAST)
-	{
-	  LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0],
-	      cli->game.y + infos[1] - j);
-	  map_tile = zappy_get_map_tile(map, cli->game.x + infos[0],
-					cli->game.y + infos[1] - j);
-	}
-      else if (infos[4] == CLI_SOUTH)
-	{
-	  LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0] - j,
-	      cli->game.y + infos[1]);
-	  map_tile = zappy_get_map_tile(map, cli->game.x + infos[0] - j,
-					cli->game.y + infos[1]);
-	}
-      else
-	{
-	  LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0],
-	      cli->game.y + infos[1] + j);
-	  map_tile = zappy_get_map_tile(map, cli->game.x + infos[0],
-					cli->game.y + infos[1] + j);
-	}
+      LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0] + j,
+	  cli->game.y + infos[1]);
+      map_tile = zappy_vision_get(map, cli, j, infos);
       memcpy(vis->map[ndx].res, map_tile->content,
 	     sizeof(vis->map[infos[3] + j].res));
       vis->map[ndx].players = map_tile->nb_players;
