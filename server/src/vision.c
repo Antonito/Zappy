e7078489e7db3a329de2ea2874625d65930a7daa
Antoine Baché <antoine.bache@epitech.eu>
2017-07-02 20:54:36 +0200

    Fixed vision

--- server/src/vision.c
+++ server/src/vision.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.bache@epitech.net>
 **
 ** Started on  Mon Jun 26 17:31:40 2017 Antoine Baché
-** Last update Thu Jun 29 18:23:27 2017 Antoine Baché
+** Last update Sun Jul  2 20:54:26 2017 Antoine Baché
 */
 
 #include <assert.h>
@@ -14,6 +14,24 @@
 #include "zappy_client.h"
 #include "zappy_map.h"
 #include "zappy_client_vision.h"
+
+static t_zappy_map_tile const	*zappy_vision_get(t_zappy_map const * const map,
+						  t_zappy_client const * const cli,
+						  int32_t j,
+						  int32_t infos[])
+{
+  if (infos[4] == CLI_NORTH)
+    return (zappy_get_map_tile(map, cli->game.x + infos[0] + j,
+			       cli->game.y + infos[1]));
+  else if (infos[4] == CLI_EAST)
+    return (zappy_get_map_tile(map, cli->game.x + infos[0],
+			       cli->game.y + infos[1] - j));
+  else if (infos[4] == CLI_SOUTH)
+    return (zappy_get_map_tile(map, cli->game.x + infos[0] - j,
+			       cli->game.y + infos[1]));
+  return (zappy_get_map_tile(map, cli->game.x + infos[0],
+			     cli->game.y + infos[1] + j));
+}
 
 static void			zappy_vision_line(t_zappy_map const *
 						  const map,
@@ -36,8 +54,7 @@ static void			zappy_vision_line(t_zappy_map const *
       LOG(LOG_DEBUG, "=== VISION ===");
       LOG(LOG_DEBUG, "tile %dx%d", cli->game.x + infos[0] + j,
 	  cli->game.y + infos[1]);
-      map_tile = zappy_get_map_tile(map, cli->game.x + infos[0] + j,
-				    cli->game.y + infos[1]);
+      map_tile = zappy_vision_get(map, cli, j, infos);
       memcpy(vis->map[ndx].res, map_tile->content,
 	     sizeof(vis->map[infos[3] + j].res));
       vis->map[ndx].players = map_tile->nb_players;
@@ -69,7 +86,7 @@ void				zappy_vision_exec(t_zappy_map const *
 			(int32_t[]){ off[!(orient == CLI_NORTH ||
 					  orient == CLI_SOUTH)],
 			    off[orient == CLI_NORTH || orient == CLI_SOUTH],
-			    off[2], i });
+			    off[2], i, orient });
       off[0] += (orient == CLI_NORTH || orient == CLI_WEST) ? -1 : 1;
       off[1] += (orient == CLI_NORTH || orient == CLI_EAST) ? 1 : -1;
       off[2] += 2;
